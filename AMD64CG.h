3621636cf9f04569ea15f951ad796d17c47af364
EfeYks534 <yefeyks@gmail.com>
2022-10-06 18:28:33 +0300

    Added documentation

--- AMD64CG.h
+++ AMD64CG.h
@@ -30,12 +30,13 @@
 #define MOD_RBP 2
 
 
-struct BinLocator
-{
-	uint32_t type;
-	uint32_t addr;
-};
-
+/* The `mod` field specifies the addressing mode.
+ *  MOD_DIR is register-direct addressing
+ *  MOD_IND is register-indirect addressing
+ *  MOD_RBP is a 8/32 bit displacement off of RBP
+ * The `r` and `rm` fields specify the reg and reg/mem fields of Mod/RM.
+ * The `im` field is an immediate, the size is deduced from the instruction identifier.
+ */
 struct BinIArg
 {
 	uint8_t   mod;
@@ -47,3 +48,34 @@ struct BinIArg
 
 
 int BinGenInstr(uint8_t **bin, struct BinIArg *arg, const char *ident);
+/* `*bin` is the code buffer. It is incremented by the size of the instruction written.
+ * `*bin` must be able to hold at least 15 bytes.  `*arg` is the instruction arguments.
+ * `arg->r` is ignored if '/digit' is in the instruction identifier. `im` is ignored if
+ * there is no ib/iw/id/iq in the instruction identifier and the addressing mode is not
+ * MOD_RBP.
+
+ * `ident` is the instruction identifier string.
+ * It is in the form of "MNEMONIC | OPCODE"
+ * The instruction mnemonic is in the form of "INSTR_NAME arg1 arg2"
+ * arg1 and arg2 are optional 2 character strings. The first character is the argument
+ * kind, the second character is the type. Arguments are separated with space.
+ * The argument kind is one of r/m/i (reg / reg/mem / immediate).
+ * The argument type is one of b/w/d/q (BYTE/WORD/DWORD/QWORD).
+ * Below is the identifier for the ADD reg/mem64, imm8 instruction.
+ *   "ADD rq ib | 83 /0 ib"
+ * The opcode is how the instruction is represented in memory. It is separated into
+ * multiple opcode-bytes. Each one is represented by a word separated with space.
+ * Below are the possible opcode-byte strings.
+ *  "XX"     - A single byte hexadecimal integer that is placed on memory.
+ *  "/r"     - Represents the Mod/RM byte.
+ *  "/digit" - Same as /r, but the reg field is replaced by the digit (0 <= digit <= 7)
+ *  "it"     - Represents a (multi)byte immediate. 't' is the type (b/w/d/q).
+ *  "+r"     - Adds `arg->r` to the last byte on memory
+ *  "L"      - Puts the legacy prefix (0x66) on memory
+ *  "R"      - Enables the REX prefix. Mod/RM can change it based on reg and reg/mem
+ *  "W"      - Enables the REX prefix and sets the W bit, making it 64 bit.
+ * Below is the identifier for the POP reg64 instruction.
+ *   "POP rq | 58 +rq"
+
+ * The return value is 0 for failure and 1 for success. 0 means no bytes were written.
+ */
